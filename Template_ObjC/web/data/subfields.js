e85abd390e3bc9a891ea233cca99d34b3f6b7e45
davefung <davefung@hkbu.edu.hk>
2018-07-18 15:46:34 +0800

    Finally

--- Template_ObjC/web/data/subfields.js
+++ Template_ObjC/web/data/subfields.js
@@ -1,2 +1,2 @@
-subfields={"Subfield1":[{"key":"SYN","name":"Syntax"},{"key":"PRG","name":"Pragmatics"}],
-"Subfield2":["SYN","PRG"]}
+subfields={"Subfield1":[{"key":"SYN","name":"Category 1"},{"key":"PRG","name":"Category 2"},{"key":"ABC","name":"Category 1"},{"key":"CDE","name":"Category 2"}],
+"Subfield2":["SYN","PRG","ABC","CDE"]}
